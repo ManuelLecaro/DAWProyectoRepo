968b846cb6156263500772b4c53b6a7102b3b97e
Manuel Lecaro <mnlecaro@gmail.com>
2018-11-29 18:51:31 -0500

    agregando timeline

--- proyecto/js/carousel.js
+++ proyecto/js/carousel.js
@@ -28,7 +28,7 @@ var max = $('#c > div').length;
 			i = 0;
 		}
 
-		$("#c > div").eq(i).css('left','0%').addClass('active').css('transition-delay','0.25s');
+		$("#c > div").eq(i).css('left','0').addClass('active').css('transition-delay','0.25s');
 		$("#c > div").eq(i + 1).css('left','25%').addClass('active').css('transition-delay','0.5s');
 		$("#c > div").eq(i + 2).css('left','50%').addClass('active').css('transition-delay','0.75s');
 		$("#c > div").eq(i + 3).css('left','75%').addClass('active').css('transition-delay','1s');
