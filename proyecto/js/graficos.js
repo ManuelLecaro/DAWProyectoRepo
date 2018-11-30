73c771c44dbee54d61b185b952df84a3dd7cb8a1
Manuel Lecaro <mnlecaro@gmail.com>
2018-11-30 09:53:49 -0500

    Cambios en busqueda

--- proyecto/js/graficos.js
+++ proyecto/js/graficos.js
@@ -8,6 +8,6 @@ d3.select(".estadisticas")
   .selectAll("div")
     .data(data)
   .enter().append("div")
-    .style("width", function(d) { return x(d) * 5 + "px"; })
+    .style("width", function(d) { return x(d) * 3 + "px"; })
     .style("background-color","rgb(135, 106, 97)")
     .text(function(d) { return d; });
